543c68b7170dea8da6014c915c52611a9af95f1a
Lukas Kämmerling <kontakt@lukas-kaemmerling.de>
2018-01-17 12:43:01 +0100

    better proxy from client errors

--- routes/web.php
+++ routes/web.php
@@ -16,68 +16,87 @@ $router->get('/', function () {
 $router->get('/api/me', function (\Illuminate\Http\Request $request) {
     if ($request->hasHeader('Authorization')) {
         $guzzle = new \GuzzleHttp\Client();
-
-        return response((string)$guzzle->get('https://ohdearapp.com/api/me', [
-            'headers' => [
-                'Authorization' => $request->header('Authorization'),
-            ],
-        ])->getBody());
+        try {
+            return response((string)$guzzle->get('https://ohdearapp.com/api/me', [
+                'headers' => [
+                    'Authorization' => $request->header('Authorization'),
+                ],
+            ])->getBody());
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return response()->make((string)$e->getResponse()->getBody());
+        }
     }
 });
 $router->get('/api/sites', function (\Illuminate\Http\Request $request) {
     if ($request->hasHeader('Authorization')) {
         $guzzle = new \GuzzleHttp\Client();
-
-        return response((string)$guzzle->get('https://ohdearapp.com/api/sites', [
-            'headers' => [
-                'Authorization' => $request->header('Authorization'),
-            ],
-        ])->getBody());
+        try {
+            return response((string)$guzzle->get('https://ohdearapp.com/api/sites', [
+                'headers' => [
+                    'Authorization' => $request->header('Authorization'),
+                ],
+            ])->getBody());
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return response()->make((string)$e->getResponse()->getBody());
+        }
     }
 });
 $router->post('/api/sites', function (\Illuminate\Http\Request $request) {
     if ($request->hasHeader('Authorization')) {
         $guzzle = new \GuzzleHttp\Client();
-
-        return response((string)$guzzle->post('https://ohdearapp.com/api/sites', [
-            'headers' => [
-                'Authorization' => $request->header('Authorization'),
-            ],
-            'json' => ['team_id' => $request->get('team_id'), 'url' => $request->get('url')],
-        ])->getBody());
+        try {
+            return response((string)$guzzle->post('https://ohdearapp.com/api/sites', [
+                'headers' => [
+                    'Authorization' => $request->header('Authorization'),
+                ],
+                'json' => ['team_id' => $request->get('team_id'), 'url' => $request->get('url')],
+            ])->getBody());
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return response()->make((string)$e->getResponse()->getBody());
+        }
     }
 });
 $router->delete('/api/sites/{site}', function (\Illuminate\Http\Request $request, $site) {
     if ($request->hasHeader('Authorization')) {
         $guzzle = new \GuzzleHttp\Client();
-
-        return response((string)$guzzle->delete('https://ohdearapp.com/api/sites/' . $site, [
-            'headers' => [
-                'Authorization' => $request->header('Authorization'),
-            ],
-            'json' => ['team_id' => $request->get('team_id'), 'url' => $request->get('url')],
-        ])->getBody());
+        try {
+            return response((string)$guzzle->delete('https://ohdearapp.com/api/sites/' . $site, [
+                'headers' => [
+                    'Authorization' => $request->header('Authorization'),
+                ],
+                'json' => ['team_id' => $request->get('team_id'), 'url' => $request->get('url')],
+            ])->getBody());
+        } catch
+        (\GuzzleHttp\Exception\ClientException $e) {
+            return response()->make((string)$e->getResponse()->getBody());
+        }
     }
 });
 $router->post('/api/checks/{check}/enable', function (\Illuminate\Http\Request $request, $check) {
     if ($request->hasHeader('Authorization')) {
         $guzzle = new \GuzzleHttp\Client();
-
-        return response((string)$guzzle->post('https://ohdearapp.com/api/checks/' . $check . '/enable', [
-            'headers' => [
-                'Authorization' => $request->header('Authorization'),
-            ],
-        ])->getBody());
+        try {
+            return response((string)$guzzle->post('https://ohdearapp.com/api/checks/' . $check . '/enable', [
+                'headers' => [
+                    'Authorization' => $request->header('Authorization'),
+                ],
+            ])->getBody());
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return response()->make((string)$e->getResponse()->getBody());
+        }
     }
 });
 $router->post('/api/checks/{check}/disable', function (\Illuminate\Http\Request $request, $check) {
     if ($request->hasHeader('Authorization')) {
         $guzzle = new \GuzzleHttp\Client();
-
-        return response((string)$guzzle->post('https://ohdearapp.com/api/checks/' . $check . '/disable', [
-            'headers' => [
-                'Authorization' => $request->header('Authorization'),
-            ],
-        ])->getBody());
+        try {
+            return response((string)$guzzle->post('https://ohdearapp.com/api/checks/' . $check . '/disable', [
+                'headers' => [
+                    'Authorization' => $request->header('Authorization'),
+                ],
+            ])->getBody());
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return response()->make((string)$e->getResponse()->getBody());
+        }
     }
 });
